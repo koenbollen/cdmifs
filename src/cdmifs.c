5b4296a4960934c75944fbded10f1f0276b60423
Koen Bollen <koenb@area51.ka.sara.nl>
2010-04-06 16:10:01 +0200

    added libcurl to the autotools with a small test

--- src/cdmifs.c
+++ src/cdmifs.c
@@ -1,8 +1,23 @@
 
-#include <stdio.h>
+#ifdef HAVE_CONFIG 
+# include <config.h>
+#endif 
+ 
+#include <stdio.h> 
+#include <curl/curl.h>
 
 int main( int argc, char *argv[] ) 
-{
-	printf( "cdmifs\n" );
+{ 
+	printf( "cdmifs\n" ); 
+	CURL *curl; 
+	CURLcode res; 
+ 
+	curl = curl_easy_init(); 
+	if(curl) { 
+		curl_easy_setopt(curl, CURLOPT_URL, "koen.it/ip/curltest");
+		res = curl_easy_perform(curl); 
+ 
+		curl_easy_cleanup(curl); 
+	} 
 	return 0; 
-}
+} 
