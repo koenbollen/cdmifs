207ef74d2f7f71863cd41cf9cc06fedf389f9e74
Koen Bollen <meneer@koenbollen.nl>
2010-04-07 14:38:19 +0200

    added util.h

---
+++ src/util.h
@@ -0,0 +1,12 @@
+/* cdmifs, a filesystem interface based on the CDMI standard.
+ * Koen Bollen <meneer koenbollen nl> 
+ * 2010 Sara
+ */
+#ifndef UTIL_H 
+#define UTIL_H 1 
+
+#include <sys/types.h>
+
+extern void *alloc( void *ptr, size_t size ) ; 
+ 
+#endif /* !UTIL_H */ 
